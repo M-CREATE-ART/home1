9732d06be6d362c8078445dfd0026fa514a6ded5
Mariam Memmedzadeh <memmedzademeryem24@gmail.com>
2020-02-19 19:23:40 +0400

    HappyToCod

--- src/main/java/HappyToCod/Animal.java
+++ src/main/java/HappyToCod/Animal.java
@@ -2,7 +2,7 @@ package HappyToCod;
 
 public class Animal {
     public static void main(String[] args) {
-        System.out.println("I'm an Animal");
+        System.out.println("I'm an Animal ");
 
 
 
